ed2a878b9b26f48692ec23a9f2c7d70001a86882
dixie24 <rpamg255@gmail.com>
2026-05-14 00:13:38 +0600

    v0.3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 41; i <= 59; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i + i);
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Простой калькулятор ===");
+
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите оператор (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.printf("%.2f + %.2f = %.2f%n", num1, num2, result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.printf("%.2f - %.2f = %.2f%n", num1, num2, result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.printf("%.2f * %.2f = %.2f%n", num1, num2, result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.printf("%.2f / %.2f = %.2f%n", num1, num2, result);
+                } else {
+                    System.out.println("Ошибка: Деление на ноль невозможно!");
+                }
+                break;
+            default:
+                System.out.println("Ошибка: Неверный оператор.");
+                break;
         }
+
+        scanner.close();
     }
 }
