e51fce74534e073480b6582feb95e0e1efd83e41
dixie24 <rpamg255@gmail.com>
2026-05-20 16:19:28 +0600

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,7 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=== Простой калькулятор ===");
-
-        System.out.print("Введите первое число: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Введите оператор (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
-
-        System.out.print("Введите второе число: ");
-        double num2 = scanner.nextDouble();
-
-        double result;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.printf("%.2f + %.2f = %.2f%n", num1, num2, result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.printf("%.2f - %.2f = %.2f%n", num1, num2, result);
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.printf("%.2f * %.2f = %.2f%n", num1, num2, result);
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    System.out.printf("%.2f / %.2f = %.2f%n", num1, num2, result);
-                } else {
-                    System.out.println("Ошибка: Деление на ноль невозможно!");
-                }
-                break;
-            default:
-                System.out.println("Ошибка: Неверный оператор.");
-                break;
-        }
-
-        scanner.close();
+        System.out.println("Hallo");
     }
 }
