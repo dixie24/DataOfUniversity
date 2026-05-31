5ea8ba84b23ce80534944f9cc81054b353a94e08
dixie24 <rpamg255@gmail.com>
2026-05-31 23:19:25 +0600

    v1.4

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,22 @@
 //        int result = calc.add(5, 7);
 //        System.out.println(result);
 //    }
+
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//        Calcul calc = new Calcul();
+//        int result = calc.add(3, 8);
+//        System.out.println(result);
+//    }
 //}
 
 public class Main {
     public static void main(String[] args) {
-        Calcul calc = new Calcul();
-        int result = calc.add(3, 8);
-        System.out.println(result);
+        
+    }
+    public class add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
     }
 }
